3b927e689bf40954182cd2f903615c4af56cdb48
hjkim <hjhjgudwls1022@gmail.com>
2024-11-06 02:22:58 +0900

    feat: Cart 및 CartItem 관련 DTO 생성

---
+++ src/main/java/com/ecommerce/domain/dto/cartItem/UpdateCartItemDto.java
@@ -0,0 +1,21 @@
+package com.ecommerce.domain.dto.cartItem;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateCartItemDto {
+
+  @NotNull(message = "상품 수량은 필수입니다.")
+  @Min(value = 1, message = "상품 수량은 1 이상이어야 합니다.")
+  private Integer quantity;
+}
