62ab5f712bd62daa31bc99b700991fa6570a98c9
hjkim <hjhjgudwls1022@gmail.com>
2024-11-14 03:17:11 +0900

    refactor: 'order' 테이블명 '@Table(name = orders)'로 변경하여 예약어 충돌 해결

--- src/main/java/com/ecommerce/domain/order/Order.java
+++ src/main/java/com/ecommerce/domain/order/Order.java
@@ -15,6 +15,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +26,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "orders")
 @Getter
 @Setter
 @Builder
